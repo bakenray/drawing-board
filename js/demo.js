f55d8b5552cf948362989aa2d30b1b04fc74fc70
bakenray <lpqstrong@163.com>
2018-06-29 17:44:45 +0800

    移动端兼容

--- js/demo.js
+++ js/demo.js
@@ -9,21 +9,24 @@ function listenToMouse(canvas){
   var usingMouse = false //鼠标触发时间默认设置false
   var lastPoint = {'x':undefined,'y':undefined} //鼠标最后点击的点坐标初始化
   
-  canvas.onmousedown = function(a){ //鼠标按下事件
-    var x =a.clientX
-    var y =a.clientY
+
+if(document.body.ontouchstart !== undefined){  //判断设备是否支持touch事件
+
+  canvas.ontouchstart = function(a){ //touch开始
+    console.log(a)
+    var x =a.touches[0].clientX
+    var y =a.touches[0].clientY
     usingMouse = true
     if(eraserEnable){
       ctx.clearRect(x-10,y-10,20,20);
     }
     else{
       lastPoint = {'x':x,'y':y}
-      // drawPoint(x,y,1)
     }
   }
-  canvas.onmousemove =function(a){  //鼠标移动事件
-    var x =a.clientX
-    var y =a.clientY
+  canvas.ontouchmove =function(a){  //touch移动
+    var x =a.touches[0].clientX
+    var y =a.touches[0].clientY
     if(!usingMouse){ return }
 
     if(eraserEnable){
@@ -31,14 +34,48 @@ function listenToMouse(canvas){
     }
     else{
         var newPoint ={'x':x,'y':y}
-        // drawPoint(x,y,1)
         drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
         lastPoint = newPoint
     }
   }
-  canvas.onmouseup =function(){  //鼠标松开事件
+  canvas.ontouchend =function(){  //touch结束
     usingMouse = false
   }
+}
+else{                   //如果不支持touch事件，则执行鼠标事件
+   canvas.onmousedown = function(a){ //鼠标按下事件
+     var x =a.clientX
+     var y =a.clientY
+     usingMouse = true
+     if(eraserEnable){
+       ctx.clearRect(x-10,y-10,20,20);
+     }
+     else{
+       lastPoint = {'x':x,'y':y}
+       // drawPoint(x,y,1)
+     }
+   }
+   canvas.onmousemove =function(a){  //鼠标移动事件
+     var x =a.clientX
+     var y =a.clientY
+     if(!usingMouse){ return }
+
+     if(eraserEnable){
+       ctx.clearRect(x-10,y-10,20,20);
+     }
+     else{
+         var newPoint ={'x':x,'y':y}
+         // drawPoint(x,y,1)
+         drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+         lastPoint = newPoint
+     }
+   }
+   canvas.onmouseup =function(){  //鼠标松开事件
+     usingMouse = false
+   }
+  }
+
+
 } 
 
 function drawPoint(x,y,radius){
